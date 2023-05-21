8099b0541fc651cda68bf203ad0194dab33d2623
Mohammed AlSammak <msk170a@gmail.com>
2023-05-22 00:05:58 +0300

    little proccess

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	file_name := os.Args[1:]
-	dat,_ := os.ReadFile(string(file_name[0]))
-	fmt.Print(dat)
+	file_name := os.Args[1:]						// Reading the first argument after program name
+	dat,_ := os.ReadFile(string(file_name[0]))		// Read the file and get the data as string
+	string_list := strings.Split(string(dat), " ")	// Conv string to a List
+	fmt.Println(string_list)
 }
