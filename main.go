843a765ebc3cef07b027e59646e344a373f1d7ee
MSK17A <legendary512@gmail.com>
2023-05-22 01:58:39 +0300

    added hex and bin functionality

--- main.go
+++ main.go
@@ -3,12 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	file_name := os.Args[1:]						// Reading the first argument after program name
-	dat,_ := os.ReadFile(string(file_name[0]))		// Read the file and get the data as string
-	string_list := strings.Split(string(dat), " ")	// Conv string to a List
+	file_name := os.Args[1:]                       // Reading the first argument after program name
+	dat, _ := os.ReadFile(string(file_name[0]))    // Read the file and get the data as byte[]
+	string_list := strings.Split(string(dat), " ") // Conv string to a List of strings
 	fmt.Println(string_list)
+
+	for idx, word := range string_list {
+		switch word {
+		case "(cap)":
+			{
+				str := string_list[idx-1]
+				string_list[idx-1] = strings.ToUpper(string(str[0])) + str[1:]
+				string_list[idx] = ""
+			}
+		case "(up)":
+			{
+				string_list[idx-1] = strings.ToUpper(string_list[idx-1])
+				string_list[idx] = ""
+			}
+		case "(low)":
+			{
+				string_list[idx-1] = strings.ToLower(string_list[idx-1])
+				string_list[idx] = ""
+			}
+		case "(hex)":
+			{
+				num, _ := strconv.ParseInt(string_list[idx-1], 16, 64)
+				string_list[idx-1] = strconv.Itoa(int(num))
+				string_list[idx] = ""
+			}
+		case "(bin)":
+			{
+				num, _ := strconv.ParseInt(string_list[idx-1], 2, 64)
+				string_list[idx-1] = strconv.Itoa(int(num))
+				string_list[idx] = ""
+			}
+		}
+	}
+	fmt.Println("")
+	fmt.Println(string_list)
+
 }
