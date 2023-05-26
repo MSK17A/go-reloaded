24a2e57c23cd769fd1e15454eebb49be0cd74607
M-SK <legendary512@gmail.com>
2023-05-26 13:00:28 +0300

    Added some text manioulation functions using by regexp

--- main.go
+++ main.go
@@ -3,34 +3,14 @@ package main
 import (
 	"fmt"
 	"regexp"
-<<<<<<< HEAD
-	//"strconv"
-=======
 	"strconv"
 
 	//"strconv"
 	"os"
->>>>>>> 6387139 (Added some text manioulation functions using by regexp)
 	"strings"
-	"os"
 )
 
 func main() {
-<<<<<<< HEAD
-	file_name := os.Args[1]                        // Reading the first argument after program name
-	dat, _ := os.ReadFile(file_name)               // Read the file and get the data as byte[]
-	str := string(dat)
-
-	reCap := regexp.MustCompile(`([^[:space:]]+)\s+\(cap\)`) // 
-	modified := reCap.ReplaceAllStringFunc(str, capitalize)
-
-	reUp := regexp.MustCompile(`([^\s]+)\s+\(up\)`)
-	modified = reUp.ReplaceAllStringFunc(modified, ToUp)
-
-	reQaws := regexp.MustCompile(`\s*\((\w+),\s*(\d+)\)`) // spcae (word, number)
-	modified = reQaws.ReplaceAllStringFunc(modified, qawsHandler)
-	fmt.Println(modified)
-=======
 	file_name := os.Args[1]          // Reading the first argument after program name
 	dat, _ := os.ReadFile(file_name) // Read the file and get the data as byte[]
 	str := string(dat)
@@ -83,20 +63,5 @@ func hex_to_dec(match string) string {
 
 func qawsHandler(match string) string {
 	fmt.Println(match[0:len(match)])
-	return "QAWS"
->>>>>>> 6387139 (Added some text manioulation functions using by regexp)
-}
-
-
-func capitalize(match string) string {
-	return strings.Title(match[0 : len(match)-6])
-}
-
-func ToUp(match string) string {
-	return strings.ToUpper(match[0 : len(match)-5])
-}
-
-func qawsHandler(match string) string {
-	fmt.Println(match[0: len(match)-])
 	return "QAWS"
 }
